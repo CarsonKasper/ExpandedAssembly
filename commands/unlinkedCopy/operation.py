0dd66d58708b3feca208d6d157222aa2ec28d9a6
Carson Kasper <carson.akasper@gmail.com>
2025-04-03 10:35:20 -0700

    started unlinked copy

--- commands/unlinkedCopy/operation.py
+++ commands/unlinkedCopy/operation.py
@@ -4,3 +4,8 @@ def run_operation():
     app = adsk.core.Application.get()
     ui = app.userInterface
     ui.messageBox("You clicked the Unlinked Copy button!")
+    
+def on_command_created(command: adsk.core.Command, inputs: adsk.core.CommandInputs):
+    select_input = inputs.addSelectionInput('target_component', 'Select Component', 'Choose the component to copy')
+    select_input.addSelectionFilter('Occurrences')
+    select_input.setSelectionLimits(1, 1)
